d5ae658488d009294e77d973283f3aa9a8822f9b
shipship <chj4119255@qq.com>
2025-01-17 13:27:17 +0800

    message check

--- pages/registor/registor.js
+++ pages/registor/registor.js
@@ -1,1 +1,22 @@
-Page({});
+Page({
+  data: {
+    username: '',
+    password: ''
+  },
+  handleInputChange: function (e) {
+    const { name, value } = e.detail;
+    this.setData({
+      [name]: value
+    });
+  },
+  handleRegister: function () {
+    const { username, password } = this.data;
+    // 存储用户名和密码到本地存储
+    wx.setStorageSync('username', username);
+    wx.setStorageSync('password', password);
+    // 跳转回登录页面
+    wx.navigateTo({
+      url: '/pages/index/index'
+    });
+  }
+});
