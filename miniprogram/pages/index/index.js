537ac964962d12c2faabc04c13a3ba99ab7b6602
shipship <chj4119255@qq.com>
2025-04-22 20:01:31 +0800

    登录页面完善

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,38 +1,92 @@
 Page({
   data: {
     username: '',
+    email: '',
     password: '',
+    code: '',
+    countdown: 0,
+    timer: null
   },
 
-  handleInputChange: function (e) {
-    console.log(e);
+  handleInputChange: function(e) {
     const { value } = e.detail;
-    const {name  } = e.target.dataset;
-    console.log(e.target.dataset);
+    const { name } = e.currentTarget.dataset;
     this.setData({
       [name]: value
     });
   },
-  // handleLogin:function(){
-  //   wx.request({
-  //     url: 'flask接口域名',
-  //     method:'POST',
-  //     data:{
-  //       username:this.data.username,
-  //       password:this.data.password
-  //     },
-  //     success(res){
-  //       console.log(res);                                     //等待后端进度
-  //       if(res.等待更改==="登录成功"){
-  //         wx.switchTab({url: '/pages/warning/warning'});
-  //       } else {
-  //         wx.showToast({title:"不存在该用户！",icon:'error'})
-  //       }
-  //     }
-  //   })
-  // },
-  handleLogin: function () {
+
+  getVerificationCode: function() {
+    if (!this.data.email) {
+      wx.showToast({
+        title: '请输入邮箱地址',
+        icon: 'none'
+      });
+      return;
+    }
+    
+    // 验证邮箱格式
+    const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailReg.test(this.data.email)) {
+      wx.showToast({
+        title: '邮箱格式不正确',
+        icon: 'none'
+      });
+      return;
+    }
+    
+    // 发送验证码请求
+    wx.request({
+      url: '你的后端API地址/send-code',
+      method: 'POST',
+      data: {
+        email: this.data.email
+      },
+      success: (res) => {
+        if (res.data.success) {
+          wx.showToast({
+            title: '验证码已发送',
+            icon: 'success'
+          });
+          this.startCountdown();
+        } else {
+          wx.showToast({
+            title: res.data.message || '发送失败',
+            icon: 'none'
+          });
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none'
+        });
+      }
+    });
+  },
+
+  startCountdown: function() {
+    this.setData({ countdown: 60 });
+    this.data.timer = setInterval(() => {
+      if (this.data.countdown <= 0) {
+        clearInterval(this.data.timer);
+        return;
+      }
+      this.setData({ countdown: this.data.countdown - 1 });
+    }, 1000);
+  },
+
+  handleLogin: function() {
     const { username, password } = this.data;
+    
+    if (!username || !password) {
+      wx.showToast({
+        title: '请填写用户名和密码',
+        icon: 'none'
+      });
+      return;
+    }
+    
     // 从本地存储中获取注册时存储的用户名和密码
     const storedUsername = wx.getStorageSync('username');
     const storedPassword = wx.getStorageSync('password');
@@ -43,7 +97,16 @@ Page({
         url: '/pages/monitor/monitor'
       });
     } else {
-      wx.showToast({	title:"不存在该用户！",icon:'error'})
+      wx.showToast({
+        title: "不存在该用户！",
+        icon: 'error'
+      });
+    }
+  },
+
+  onUnload: function() {
+    if (this.data.timer) {
+      clearInterval(this.data.timer);
     }
   }
 });
--- miniprogram/pages/registor/registor.js
+++ miniprogram/pages/registor/registor.js
@@ -1,83 +1,84 @@
 Page({
   data: {
-    username: '',
-    password: '',
-    isModify: false // 是否来自修改模式
+      username: '',
+      password: '',
+      isModify: false // 是否来自修改模式
   },
 
   onLoad: function(options) {
-    // 判断是否来自修改模式
-    if (options.modify) {
-      this.setData({
-        isModify: true,
-        username: wx.getStorageSync('username') || '',
-        password: wx.getStorageSync('password') || ''
-      });
-    }
+      // 判断是否来自修改模式
+      if (options.modify) {
+          this.setData({
+              isModify: true,
+              username: wx.getStorageSync('username') || '',
+              password: wx.getStorageSync('password') || ''
+          });
+      }
   },
 
   handleInputChange: function(e) {
-    const { value } = e.detail;
-    const { name } = e.target.dataset;
-    this.setData({
-      [name]: value
-    });
+      const { value } = e.detail;
+      const { name } = e.target.dataset;
+      this.setData({
+          [name]: value
+      });
   },
 
   handleRegister: function() {
-    const { username, password } = this.data;
-    if (!username || !password) {
-      wx.showToast({ title: '请填写完整信息', icon: 'none' });
-      return;
-    }
-    
-    // 保存用户名和密码到缓存
-    wx.setStorageSync('username', username);
-    wx.setStorageSync('password', password);
-    
-    wx.cloud.callFunction({
-      name: 'register',
-      data: { username, password },
-      success: (res) => {
-        console.log(res,22);
-        if (res.result.code == 0) {
-          wx.navigateBack();
-          wx.showToast({ title: res.result.message });
-        } else {
-          wx.showToast({ title: res.result.message, icon: 'none' });
-        }
-      },
-      fail: (err) => {
-        wx.showToast({ title: '网络错误', icon: 'none' });
+      const { username, password } = this.data;
+      if (!username || !password) {
+          wx.showToast({ title: '请填写完整信息', icon: 'none' });
+          return;
       }
-    });
+
+      // 保存用户名和密码到缓存
+      wx.setStorageSync('username', username);
+      wx.setStorageSync('password', password);
+
+      wx.cloud.callFunction({
+          name: 'register',
+          data: { username, password },
+          success: (res) => {
+              console.log(res, 22);
+              if (res.result.code == 0) {
+                  wx.navigateBack();
+                  wx.showToast({ title: res.result.message });
+              } else {
+                  wx.showToast({ title: res.result.message, icon: 'none' });
+              }
+          },
+          fail: (err) => {
+              wx.showToast({ title: '网络错误', icon: 'none' });
+          }
+      });
   },
 
   // 修改按钮点击事件
   handleModify: function() {
-    const { username, password } = this.data;
-    if (!username || !password) {
-      wx.showToast({ title: '请填写完整信息', icon: 'none' });
-      return;
-    }
-    
-    // 保存修改后的用户名和密码
-    wx.setStorageSync('username', username);
-    wx.setStorageSync('password', password);
-    
-    // 返回user-center页面并传递新用户名
-    const pages = getCurrentPages();
-    const prevPage = pages[pages.length - 2]; // 上一个页面(user-center)
-    if (prevPage) {
-      prevPage.setData({
-        username: username
-      });
-    }
-    
-    wx.navigateBack({
-      success: () => {
-        wx.showToast({ title: '修改成功' });
+      const { username, password } = this.data;
+      if (!username || !password) {
+          wx.showToast({ title: '请填写完整信息', icon: 'none' });
+          return;
       }
-    });
+
+      // 保存修改后的用户名和密码
+      wx.setStorageSync('username', username);
+      wx.setStorageSync('password', password);
+
+      // 返回user - center页面并传递新用户名
+      const pages = getCurrentPages();
+      const prevPage = pages[pages.length - 2]; // 上一个页面(user - center)
+      if (prevPage) {
+          prevPage.setData({
+              username: username
+          });
+      }
+
+      wx.navigateBack({
+          success: () => {
+              wx.showToast({ title: '修改成功' });
+          }
+      });
   }
 });
+  
